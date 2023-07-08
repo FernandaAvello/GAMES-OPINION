fde0de6a0e24195f5f446a82d559574488e9ac08
Fernanda <fernandaavellorocha@gmail.com>
2023-07-08 13:08:15 -0400

    Config inicial

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import store from './store'
+import vuetify from './plugins/vuetify'
+import axios from './plugins/axios'
+import toastification from './plugins/toastificacion'
 
 Vue.config.productionTip = false
-
+Vue.prototype.$http = axios
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  vuetify,
+  axios,
+  toastification,
+  render: h => h(App)
 }).$mount('#app')
